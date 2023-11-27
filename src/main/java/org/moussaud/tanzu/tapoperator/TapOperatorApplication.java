0380b390889b1fffb6bff2865e192e430a480cd9
Benoit Moussaud <benoit@moussaud.org>
2023-11-27 15:48:34 +0100

    initial import

---
+++ src/main/java/org/moussaud/tanzu/tapoperator/TapOperatorApplication.java
@@ -0,0 +1,13 @@
+package org.moussaud.tanzu.tapoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/moussaud/tanzu/tapoperator/TapOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moussaud.tanzu.tapoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
